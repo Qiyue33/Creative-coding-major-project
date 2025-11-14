4b8fc2f973d4267f937c0d87ca7152f605d5d097
ZiyanXu0923 <zixu0982@uni.sydney.edu.au>
2025-11-14 21:39:48 +1100

    Build the basic graphic framework
    add details

--- sketch.js
+++ sketch.js
@@ -1,24 +1,17 @@
-let springColor, summerColor, autumnColor, winterColor;
-let seasons = []; 
+let seasons = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
-  springColor = color(70, 180, 100); 
-  summerColor = color(230, 80, 70); 
-  autumnColor = color(220, 180, 60); 
-  winterColor = color(70, 120, 200);  
-
-    initializeSeasons(); 
+  initializeSeasons();
   noLoop();
 }
+
 
 function draw() {
   background(100, 120, 140);
   drawTexture();
-  drawQuadrantBorders(); // Draw the quadrant border
+  drawQuadrantBorders();
 
-  
   for (let season of seasons) {
     season.draw();
   }
@@ -26,21 +19,21 @@ function draw() {
 
 // Initialize the four quadrants
 function initializeSeasons() {
+  seasons = [];
   const quadrantWidth = width / 2;
   const quadrantHeight = height / 2;
-
   seasons.push(new Spring(0, 0, quadrantWidth, quadrantHeight)); 
   seasons.push(new Summer(quadrantWidth, 0, quadrantWidth, quadrantHeight)); 
   seasons.push(new Autumn(0, quadrantHeight, quadrantWidth, quadrantHeight)); 
   seasons.push(new Winter(quadrantWidth, quadrantHeight, quadrantWidth, quadrantHeight)); 
 }
 
-// Initialize the four quadrants
+// Draw the boundary lines of the quadrants
 function drawQuadrantBorders() {
   noFill();
   stroke(255, 80);
-  line(width / 2, 0, width / 2, height); 
-  line(0, height / 2, width, height / 2); 
+  line(width / 2, 0, width / 2, height);
+  line(0, height / 2, width, height / 2);
 }
 
 // Background texture
@@ -51,19 +44,18 @@ function drawTexture() {
   }
 }
 
-
 class Season {
   constructor(x, y, width, height) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
-    this.cx = x + width / 2; 
-    this.cy = y + height / 2; 
+    this.cx = x + width / 2;
+    this.cy = y + height / 2;
   }
 
-  draw() {} 
-  doubleColorCircle(x, y, diameter, color1, color2) { 
+  draw() {}
+  doubleColorCircle(x, y, diameter, color1, color2) {
     noStroke();
     fill(color1);
     arc(x, y, diameter, diameter, HALF_PI, HALF_PI * 3, PIE);
@@ -74,25 +66,180 @@ class Season {
 
 // Spring
 class Spring extends Season {
-  draw() {}
+  draw() {
+    this.drawGround();
+    this.drawBase();
+    this.drawStem();
+    this.drawBranches();
+  }
+
+  drawGround() {
+    noStroke();
+    fill(70, 180, 100, 150);
+    rect(this.x, this.cy + 120, this.width, this.height - 360);
+  }
+
+  drawBase() {
+    noStroke();
+    fill(70, 180, 100);
+    rect(this.cx - 120, this.cy + 60, 40, 40);
+    fill(220, 180, 60);
+    rect(this.cx - 80, this.cy + 60, 80, 40);
+    fill(70, 180, 100);
+    rect(this.cx, this.cy + 60, 40, 40);
+    this.doubleColorCircle(this.cx - 60, this.cy + 100, 40, color(230, 80, 70), color(70, 180, 100));
+    this.doubleColorCircle(this.cx, this.cy + 100, 40, color(70, 180, 100), color(230, 80, 70));
+  }
+
+  drawStem() {
+    const stemY = [this.cy + 40, this.cy, this.cy - 40, this.cy - 80, this.cy - 120];
+    for (let y of stemY) {
+      this.doubleColorCircle(this.cx - 20, y, 40, color(230, 80, 70), color(70, 180, 100));
+    }
+  }
+
+  drawBranches() {
+    this.doubleColorCircle(this.cx - 80, this.cy - 40, 40, color(230, 80, 70), color(70, 180, 100));
+    this.doubleColorCircle(this.cx - 130, this.cy - 40, 40, color(70, 180, 100), color(230, 80, 70));
+    this.doubleColorCircle(this.cx + 40, this.cy - 40, 40, color(230, 80, 70), color(70, 180, 100));
+    this.doubleColorCircle(this.cx + 80, this.cy - 80, 40, color(70, 180, 100), color(230, 80, 70));
+  }
 }
 
 // Summer
 class Summer extends Season {
-  draw() {} 
+  draw() {
+    this.drawGround();
+    this.drawBase();
+    this.drawStem();
+    this.drawBranches();
+  }
+
+  drawGround() {
+    noStroke();
+    fill(255, 200, 50, 150);
+    
+    rect(this.x, this.cy + 120, this.width, this.height - 360); 
+  }
+
+  drawBase() {
+    noStroke();
+    fill(255, 140, 0);
+    rect(this.cx - 120, this.cy + 60, 40, 40);
+    fill(255, 255, 0);
+    rect(this.cx - 80, this.cy + 60, 80, 40);
+    fill(255, 140, 0);
+    rect(this.cx, this.cy + 60, 40, 40);
+    this.doubleColorCircle(this.cx - 60, this.cy + 100, 40, color(0, 100, 200), color(255, 140, 0));
+    this.doubleColorCircle(this.cx, this.cy + 100, 40, color(255, 140, 0), color(0, 100, 200));
+  }
+
+  drawStem() {
+    const stemY = [this.cy + 40, this.cy, this.cy - 40, this.cy - 80, this.cy - 120];
+    for (let y of stemY) {
+      this.doubleColorCircle(this.cx - 20, y, 40, color(0, 100, 200), color(255, 140, 0));
+    }
+  }
+
+  drawBranches() {
+    
+    this.doubleColorCircle(this.cx - 80, this.cy - 40, 40, color(0, 100, 200), color(255, 140, 0));
+    this.doubleColorCircle(this.cx - 130, this.cy - 40, 40, color(255, 140, 0), color(0, 100, 200));
+    this.doubleColorCircle(this.cx + 40, this.cy - 40, 40, color(0, 100, 200), color(255, 140, 0));
+    this.doubleColorCircle(this.cx + 90, this.cy - 80, 40, color(255, 140, 0), color(0, 100, 200));
+  }
 }
+
 
 // Autumn
 class Autumn extends Season {
-  draw() {} 
+  draw() {
+    this.drawGround();
+    this.drawBase();
+    this.drawStem();
+    this.drawBranches();
+  }
+
+  drawGround() {
+    noStroke();
+    fill(200, 100, 50, 150); 
+    rect(this.x, this.cy + 120, this.width, this.height - 360);
+  }
+
+  drawBase() {
+    noStroke();
+    fill(200, 80, 0); 
+    rect(this.cx - 120, this.cy + 60, 40, 40);
+    fill(255, 180, 50); 
+    rect(this.cx - 80, this.cy + 60, 80, 40);
+    fill(200, 80, 0); 
+    rect(this.cx, this.cy + 60, 40, 40);
+   
+    this.doubleColorCircle(this.cx - 60, this.cy + 100, 40, color(255, 200, 0), color(200, 80, 0));
+    this.doubleColorCircle(this.cx, this.cy + 100, 40, color(200, 80, 0), color(255, 200, 0));
+  }
+
+  drawStem() {
+    const stemY = [this.cy + 40, this.cy, this.cy - 40, this.cy - 80, this.cy - 120];
+    for (let y of stemY) {
+      this.doubleColorCircle(this.cx - 20, y, 40, color(255, 200, 0), color(200, 80, 0));
+    }
+  }
+
+  drawBranches() {
+   
+    this.doubleColorCircle(this.cx - 80, this.cy - 40, 40, color(255, 200, 0), color(200, 80, 0));
+    this.doubleColorCircle(this.cx - 130, this.cy - 40, 40, color(200, 80, 0), color(255, 200, 0));
+    this.doubleColorCircle(this.cx + 40, this.cy - 40, 40, color(255, 200, 0), color(200, 80, 0));
+    this.doubleColorCircle(this.cx + 80, this.cy - 80, 40, color(200, 80, 0), color(255, 200, 0));
+  }
 }
 
-// Winter
+// winter
 class Winter extends Season {
-  draw() {} 
+  draw() {
+    this.drawGround();
+    this.drawBase();
+    this.drawStem();
+    this.drawBranches();
+  }
+
+  drawGround() {
+    noStroke();
+    fill(200, 220, 255, 150); 
+    rect(this.x, this.cy + 120, this.width, this.height - 360);
+  }
+
+  drawBase() {
+    noStroke();
+    fill(50, 100, 150); 
+    rect(this.cx - 120, this.cy + 60, 40, 40);
+    fill(255, 255, 255); 
+    rect(this.cx - 80, this.cy + 60, 80, 40);
+    fill(50, 100, 150); 
+    rect(this.cx, this.cy + 60, 40, 40);
+   
+    this.doubleColorCircle(this.cx - 60, this.cy + 100, 40, color(255, 255, 255), color(50, 100, 150));
+    this.doubleColorCircle(this.cx, this.cy + 100, 40, color(50, 100, 150), color(255, 255, 255));
+  }
+
+  drawStem() {
+    const stemY = [this.cy + 40, this.cy, this.cy - 40, this.cy - 80, this.cy - 120];
+    for (let y of stemY) {
+      this.doubleColorCircle(this.cx - 20, y, 40, color(255, 255, 255), color(50, 100, 150));
+    }
+  }
+
+  drawBranches() {
+   
+    this.doubleColorCircle(this.cx - 80, this.cy - 40, 40, color(255, 255, 255), color(50, 100, 150));
+    this.doubleColorCircle(this.cx - 130, this.cy - 40, 40, color(50, 100, 150), color(255, 255, 255));
+    this.doubleColorCircle(this.cx + 40, this.cy - 40, 40, color(255, 255, 255), color(50, 100, 150));
+    this.doubleColorCircle(this.cx + 90, this.cy - 80, 40, color(50, 100, 150), color(255, 255, 255));
+  }
 }
 
-// Window adaptation
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   initializeSeasons();
