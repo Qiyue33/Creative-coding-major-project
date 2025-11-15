adef8ebddc83c4bb82602871fba79895fbfff654
Qiyue33 <qiji0008@uni.sydney.edu.au>
2025-11-15 20:40:32 +1100

    Add summer part
    Create a new Summer.js file, and supplement the relevant content.

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let seasons = [];
 let snowflakes = [];
 
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   initializeSeasons();
@@ -9,18 +10,19 @@ function setup() {
   for (let i = 0; i < 300; i++) {
     // Add a new snowflake object to the array
     snowflakes.push(new Snowflake());
-}}
+  }
+}
 
 function draw() {
   background(100, 120, 140);
   drawTexture();
   drawQuadrantBorders();
- 
+
 
   for (let season of seasons) {
     season.draw();
- }
- 
+  }
+
   // Update and display each snowflake in the array
   let currentTime = frameCount / 60;
 
@@ -37,10 +39,10 @@ function initializeSeasons() {
   seasons = [];
   const quadrantWidth = width / 2;
   const quadrantHeight = height / 2;
-  seasons.push(new Spring(0, 0, quadrantWidth, quadrantHeight)); 
-  seasons.push(new Summer(quadrantWidth, 0, quadrantWidth, quadrantHeight)); 
-  seasons.push(new Autumn(0, quadrantHeight, quadrantWidth, quadrantHeight)); 
-  seasons.push(new Winter(quadrantWidth, quadrantHeight, quadrantWidth, quadrantHeight)); 
+  seasons.push(new Spring(0, 0, quadrantWidth, quadrantHeight));
+  seasons.push(new Summer(quadrantWidth, 0, quadrantWidth, quadrantHeight));
+  seasons.push(new Autumn(0, quadrantHeight, quadrantWidth, quadrantHeight));
+  seasons.push(new Winter(quadrantWidth, quadrantHeight, quadrantWidth, quadrantHeight));
 }
 
 // Draw the boundary lines of the quadrants
@@ -60,50 +62,7 @@ function drawTexture() {
 }
 
 
-// Summer
-class Summer extends Season {
-  draw() {
-    this.drawGround();
-    this.drawBase();
-    this.drawStem();
-    this.drawBranches();
-  }
 
-  drawGround() {
-    noStroke();
-    fill(255, 200, 50, 150);
-    
-    rect(this.x, this.cy + 120, this.width, this.height - 360); 
-  }
-
-  drawBase() {
-    noStroke();
-    fill(255, 140, 0);
-    rect(this.cx - 120, this.cy + 60, 40, 40);
-    fill(255, 255, 0);
-    rect(this.cx - 80, this.cy + 60, 80, 40);
-    fill(255, 140, 0);
-    rect(this.cx, this.cy + 60, 40, 40);
-    this.doubleColorCircle(this.cx - 60, this.cy + 100, 40, color(0, 100, 200), color(255, 140, 0));
-    this.doubleColorCircle(this.cx, this.cy + 100, 40, color(255, 140, 0), color(0, 100, 200));
-  }
-
-  drawStem() {
-    const stemY = [this.cy + 40, this.cy, this.cy - 40, this.cy - 80, this.cy - 120];
-    for (let y of stemY) {
-      this.doubleColorCircle(this.cx - 20, y, 40, color(0, 100, 200), color(255, 140, 0));
-    }
-  }
-
-  drawBranches() {
-    
-    this.doubleColorCircle(this.cx - 80, this.cy - 40, 40, color(0, 100, 200), color(255, 140, 0));
-    this.doubleColorCircle(this.cx - 130, this.cy - 40, 40, color(255, 140, 0), color(0, 100, 200));
-    this.doubleColorCircle(this.cx + 40, this.cy - 40, 40, color(0, 100, 200), color(255, 140, 0));
-    this.doubleColorCircle(this.cx + 90, this.cy - 80, 40, color(255, 140, 0), color(0, 100, 200));
-  }
-
-}
 
 
 // winter
@@ -117,19 +76,19 @@ class Winter extends Season {
 
   drawGround() {
     noStroke();
-    fill(200, 220, 255, 150); 
+    fill(200, 220, 255, 150);
     rect(this.x, this.cy + 120, this.width, this.height - 360);
   }
 
   drawBase() {
     noStroke();
-    fill(50, 100, 150); 
+    fill(50, 100, 150);
     rect(this.cx - 120, this.cy + 60, 40, 40);
-    fill(255, 255, 255); 
+    fill(255, 255, 255);
     rect(this.cx - 80, this.cy + 60, 80, 40);
-    fill(50, 100, 150); 
+    fill(50, 100, 150);
     rect(this.cx, this.cy + 60, 40, 40);
-   
+
     this.doubleColorCircle(this.cx - 60, this.cy + 100, 40, color(255, 255, 255), color(50, 100, 150));
     this.doubleColorCircle(this.cx, this.cy + 100, 40, color(50, 100, 150), color(255, 255, 255));
   }
@@ -142,7 +101,7 @@ class Winter extends Season {
   }
 
   drawBranches() {
-   
+
     this.doubleColorCircle(this.cx - 80, this.cy - 40, 40, color(255, 255, 255), color(50, 100, 150));
     this.doubleColorCircle(this.cx - 130, this.cy - 40, 40, color(50, 100, 150), color(255, 255, 255));
     this.doubleColorCircle(this.cx + 40, this.cy - 40, 40, color(255, 255, 255), color(50, 100, 150));
